eb3734728f1a5ef96606bcbad63f7b807985a388
Luciano Iam <oss@lucianoiam.com>
2025-04-14 09:35:08 +0200

    Refactor dawscript.py

--- dawscript.py
+++ dawscript.py
@@ -5,26 +5,32 @@
 import os
 import sys
 
-try:
-    from dawscript_core.host import main
-except ModuleNotFoundError:
-    sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-    from dawscript_core.host import DawscriptControlSurface, main
 
-import controller
+# Edit as necessary
+def load_controller():
+    # Your own controller implementation
+    import controller
 
-#from dawscript_core.extra.web import controller
-#controller.set_config( ... )
+    # Default controller for web scripts
+    #from dawscript_core.extra.web import controller
+    #controller.set_config( ... )
+
+    return controller
 
 
 # Entry point for Ableton Live
 def create_instance(c_instance):
+    sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+    from dawscript_core.host import DawscriptControlSurface, main
+
     instance = DawscriptControlSurface(c_instance)
-    main(controller, instance)
+    main(load_controller(), instance)
 
     return instance
 
 
 # Entry point for REAPER and CLI
 if __name__ == "__main__":
-    main(controller, globals())
+    from dawscript_core.host import main
+
+    main(load_controller(), globals())
